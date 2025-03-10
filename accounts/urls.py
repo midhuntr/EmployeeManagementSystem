695063497eb0f4025198ca458e5e20c0e8957003
midhuntr <midhuntrmidhun@gmail.com>
2025-03-10 17:51:29 +0530

    Updated urls.py files

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ from .views import delete_employee
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
+    path('', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('change-password/', views.change_password, name='change_password'),
     
